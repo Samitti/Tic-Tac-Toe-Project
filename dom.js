f08501effa960f44348e84740c45e77e57bb8864
Mupa1 <mupasmail@gmail.com>
2020-09-09 16:57:42 +0200

    fix linter errors

--- dom.js
+++ dom.js
@@ -1,19 +1,23 @@
-const startGame = document.getElementById("startGame");
-const gameBord = document.getElementById("gameBord");
-const replayGame = document.getElementById("replayGame");
-const allcells = document.getElementsByClassName("cell");
-const nextPlayer = document.getElementById("nextPlayer");
-const playersForm = document.getElementById("playersForm");
-const playerOneName = document.getElementById("playerOneName");
-const playerTwoName = document.getElementById("playerTwoName");
+const startGame = document.getElementById('startGame');
+const gameBord = document.getElementById('gameBord');
+const replayGame = document.getElementById('replayGame');
+const allcells = document.getElementsByClassName('cell');
+const nextPlayer = document.getElementById('nextPlayer');
+const playersForm = document.getElementById('playersForm');
+const playerOneName = document.getElementById('playerOneName');
+const playerTwoName = document.getElementById('playerTwoName');
+const alertMsg = document.getElementById('alertMsg');
+const gameOver = document.getElementById('gameOver');
 
 export {
-    startGame,
-    gameBord,
-    replayGame,
-    allcells,
-    nextPlayer,
-    playersForm,
-    playerOneName,
-    playerTwoName,
+  startGame,
+  gameBord,
+  replayGame,
+  allcells,
+  nextPlayer,
+  playersForm,
+  playerOneName,
+  playerTwoName,
+  alertMsg,
+  gameOver,
 };
--- index.js
+++ index.js
@@ -1,137 +1,142 @@
 import {
-    startGame,
-    gameBord,
-    replayGame,
-    allcells,
-    nextPlayer,
-    playersForm,
+  startGame,
+  gameBord,
+  replayGame,
+  allcells,
+  nextPlayer,
+  playersForm,
+  playerOneName,
+  playerTwoName,
+  alertMsg,
+  gameOver,
+// eslint-disable-next-line import/extensions
+} from './dom.js';
+
+// eslint-disable-next-line func-names
+const gameModule = (function () {
+  const gameOn = false;
+  const playerOneName = '';
+  const playerTwoName = '';
+  const currentPlayerName = '';
+  const currentPlayerSym = '';
+  const gameBoard = ['', '', '', '', '', '', '', '', ''];
+  const count = 0;
+
+  return {
+    gameOn,
     playerOneName,
     playerTwoName,
-} from "./dom.js";
-
-const gameModule = (function () {
-    let gameOn = false;
-    let playerOneName = "";
-    let playerTwoName = "";
-    let currentPlayerName = "";
-    let currentPlayerSym = "";
-    let gameBoard = ["", "", "", "", "", "", "", "", ""];
-    let count = 0;
-
-    return {
-        gameOn: gameOn,
-        playerOneName: playerOneName,
-        playerTwoName: playerTwoName,
-        currentPlayerName: currentPlayerName,
-        currentPlayerSym: currentPlayerSym,
-        gameBoard: gameBoard,
-        count: count,
-    };
-})();
-
-const checkInput = (ev) => {
-    ev.preventDefault();
-    if (playerOneName.value === "" || playerTwoName.value === "") {
-        alert("Name can't be empty!");
-    } else {
-        gameInit(playerOneName.value, playerTwoName.value);
-    }
-};
+    currentPlayerName,
+    currentPlayerSym,
+    gameBoard,
+    count,
+  };
+}());
 
 const gameInit = (playerOneName, playerTwoName) => {
-    gameModule.gameOn = true;
-    gameModule.playerOneName = playerOneName;
-    gameModule.playerTwoName = playerTwoName;
-    gameModule.currentPlayerName = gameModule.playerOneName;
-    gameModule.currentPlayerSym = "X";
-    playersForm.style.display = "none";
-    nextPlayer.innerHTML = gameModule.currentPlayerName + " Start!";
+  gameModule.gameOn = true;
+  gameModule.playerOneName = playerOneName;
+  gameModule.playerTwoName = playerTwoName;
+  gameModule.currentPlayerName = gameModule.playerOneName;
+  gameModule.currentPlayerSym = 'X';
+  playersForm.style.display = 'none';
+  nextPlayer.innerHTML = `${gameModule.currentPlayerName} Start!`;
 };
 
-const updateMoves = (el) => {
-    if (gameModule.gameOn) {
-        el.innerHTML = gameModule.currentPlayerSym;
-        gameModule.gameBoard[el.value] = gameModule.currentPlayerSym;
-        el.disabled = true;
-        gameModule.count++;
-        checkNextMove();
-
-        nextPlayer.innerHTML = gameModule.currentPlayerName + " Turn!";
-    } else {
-        alert("Invalid Move");
-    }
+const checkInput = (ev) => {
+  ev.preventDefault();
+  if (playerOneName.value === '' || playerTwoName.value === '') {
+    alertMsg.innerHTML = 'NAME CANNOT BE BLANK!';
+  } else {
+    gameInit(playerOneName.value, playerTwoName.value);
+  }
 };
 
 const winChecker = () => {
-    let win = false;
-    let gb = gameModule.gameBoard;
-    let winCombinations = [
-        [gb[0], gb[1], gb[2]],
-        [gb[3], gb[4], gb[5]],
-        [gb[6], gb[7], gb[8]],
-        [gb[0], gb[3], gb[6]],
-        [gb[1], gb[4], gb[7]],
-        [gb[2], gb[5], gb[8]],
-        [gb[0], gb[4], gb[8]],
-        [gb[2], gb[4], gb[6]],
-    ];
-    let checkSym = (sym) => sym === gameModule.currentPlayerSym;
-    winCombinations.forEach((el) => {
-        if (el.every(checkSym)) {
-            win = true;
-        }
-    });
-    return win;
-};
-
-const drawChecker = () => {
-    if (gameModule.count > 8) {
-        return true;
-    } else {
-        return false;
+  let win = false;
+  const gb = gameModule.gameBoard;
+  const winCombinations = [
+    [gb[0], gb[1], gb[2]],
+    [gb[3], gb[4], gb[5]],
+    [gb[6], gb[7], gb[8]],
+    [gb[0], gb[3], gb[6]],
+    [gb[1], gb[4], gb[7]],
+    [gb[2], gb[5], gb[8]],
+    [gb[0], gb[4], gb[8]],
+    [gb[2], gb[4], gb[6]],
+  ];
+  const checkSym = (sym) => sym === gameModule.currentPlayerSym;
+  winCombinations.forEach((el) => {
+    if (el.every(checkSym)) {
+      win = true;
     }
+  });
+  return win;
 };
 
 const changePlayer = () => {
-    if (gameModule.currentPlayerName === gameModule.playerOneName) {
-        gameModule.currentPlayerName = gameModule.playerTwoName;
-        gameModule.currentPlayerSym = "O";
-    } else if (gameModule.currentPlayerName === gameModule.playerTwoName) {
-        gameModule.currentPlayerName = gameModule.playerOneName;
-        gameModule.currentPlayerSym = "X";
-    }
+  if (gameModule.currentPlayerName === gameModule.playerOneName) {
+    gameModule.currentPlayerName = gameModule.playerTwoName;
+    gameModule.currentPlayerSym = 'O';
+  } else if (gameModule.currentPlayerName === gameModule.playerTwoName) {
+    gameModule.currentPlayerName = gameModule.playerOneName;
+    gameModule.currentPlayerSym = 'X';
+  }
+};
+
+const drawChecker = () => {
+  if (gameModule.count > 8) {
+    return true;
+  }
+  return false;
 };
 
 const checkNextMove = () => {
-    if (winChecker()) {
-        alert(gameModule.currentPlayerName + " Wins!");
-        for (let i = 0; i < allcells.length; i++) {
-            allcells[i].disabled = true;
-        }
-        replayGame.style.display = "block";
-    } else if (drawChecker()) {
-        alert("No One Wins Game Over!");
-        replayGame.style.display = "block";
-    } else {
-        changePlayer();
+  if (winChecker()) {
+    gameOver.innerHTML = `${gameModule.currentPlayerName} Wins!`;
+    gameOver.style.display = 'block';
+    for (let i = 0; i < allcells.length; i += 1) {
+      allcells[i].disabled = true;
     }
+    replayGame.style.display = 'block';
+  } else if (drawChecker()) {
+    gameOver.innerHTML = 'No One Wins Game Over!';
+    gameOver.style.display = 'block';
+    replayGame.style.display = 'block';
+  } else {
+    changePlayer();
+  }
+};
+
+const updateMoves = (el) => {
+  if (gameModule.gameOn) {
+    el.innerHTML = gameModule.currentPlayerSym;
+    gameModule.gameBoard[el.value] = gameModule.currentPlayerSym;
+    el.disabled = true;
+    gameModule.count += 1;
+    checkNextMove();
+    nextPlayer.innerHTML = `${gameModule.currentPlayerName} Turn!`;
+  } else {
+    alertMsg.innerHTML = 'Invalid Move';
+  }
 };
 
 const restartGame = () => {
-    gameModule.currentPlayerName = gameModule.playerOneName;
-    gameModule.currentPlayerSym = "X";
-    gameModule.count = 0;
-    gameModule.gameBoard = ["", "", "", "", "", "", "", "", ""];
+  gameModule.currentPlayerName = gameModule.playerOneName;
+  gameModule.currentPlayerSym = 'X';
+  gameModule.count = 0;
+  gameModule.gameBoard = ['', '', '', '', '', '', '', '', ''];
 
-    for (let i = 0; i < allcells.length; i++) {
-        allcells[i].disabled = false;
-        allcells[i].innerHTML = i;
-    }
-    replayGame.style.display = "none";
+  for (let i = 0; i < allcells.length; i += 1) {
+    allcells[i].disabled = false;
+    allcells[i].innerHTML = i;
+  }
+  replayGame.style.display = 'none';
+  gameOver.style.display = 'none';
 };
 
-startGame.addEventListener("click", checkInput);
-gameBord.addEventListener("click", (e) => {
-    updateMoves(e.target);
+startGame.addEventListener('click', checkInput);
+gameBord.addEventListener('click', (e) => {
+  updateMoves(e.target);
 });
-replayGame.addEventListener("click", restartGame);
+replayGame.addEventListener('click', restartGame);
